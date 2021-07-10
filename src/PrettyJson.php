8864ea4d03a79ae1be51cd77953bf89d450228c8
Chuks Okwuenu <chuksjimy@gmail.com>
2021-07-10 02:13:07 +0100

    created prettyjson.php

---
+++ src/PrettyJson.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * MajorGrey PrettyJson library
+ * 
+ * https://jdelta.github.io/pretty-json
+ * Version 1.0
+ * 
+ * Copyright 2021, Chuks Okwuenu
+ * Released under the MIT license
+ */
+
+namespace MajorGrey;
+ 
+class PrettyJson
+{
+    const SPACE = 0;
+    const FLAG = false;
+
+    public static function prettyprint($json, $json_attribute_color = 'red', $value_attribute_color = 'blue')
+    {
+        $output = stripcslashes();
+        return $output;
+    }
+}
