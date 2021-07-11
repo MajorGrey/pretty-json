eea2d5918fffd5b0d1b3abd0f76593ec1f550fd4
Chuks Okwuenu <chuksjimy@gmail.com>
2021-07-11 04:53:15 +0100

    pretty json impeleted

--- src/PrettyJson.php
+++ src/PrettyJson.php
@@ -14,12 +14,68 @@ namespace MajorGrey;
  
 class PrettyJson
 {
-    const SPACE = 0;
-    const FLAG = false;
 
     public static function prettyprint($json, $json_attribute_color = 'red', $value_attribute_color = 'blue')
     {
-        $output = stripcslashes();
+        $output = self::loop_json($json, $json_attribute_color, $value_attribute_color);
         return $output;
+    }
+
+    private static function loop_json($json,  $json_attribute_color, $value_attribute_color)
+    {
+        $space = 0;
+        $flag = false;
+        
+        $data =  "<pre>";
+
+        //loop for iterating the full json data
+        for ($counter=0; $counter<strlen($json); $counter++) {
+
+            //Checking ending second and third brackets
+            if ($json[$counter] == '}' || $json[$counter] == ']') {
+                $space--;
+                $data .=  "\n";
+                $data .=  str_repeat(' ', ($space*2));
+            }
+
+            //Checking for double quote(“) and comma (,)
+            if ($json[$counter] == '"' && ($json[$counter-1] == ',' ||
+            $json[$counter-2] == ',')) {
+                $data .=  "\n";
+                $data .=  str_repeat(' ', ($space*2));
+            }
+            
+            if ($json[$counter] == '"' && !$flag) {
+                if ($json[$counter-1] == ':' || $json[$counter-2] == ':') {
+
+                    //Add formatting for question and answer
+                    $data .=  "<span style='color:$value_attribute_color'>";
+                } else {
+
+                    //Add formatting for answer options
+                    $data .=  "<span style='color:$json_attribute_color;'>";
+                }
+            }
+
+            $data .=  $json[$counter];
+            //Checking conditions for adding closing span tag
+            if ($json[$counter] == '"' && $flag) {
+                $data .=  '</span>';
+            }
+            
+            if ($json[$counter] == '"') {
+                $flag = !$flag;
+            }
+
+            //Checking starting second and third brackets
+            if ($json[$counter] == '{' || $json[$counter] == '[') {
+                $space++;
+                $data .=  "\n";
+                $data .=  str_repeat(' ', ($space*2));
+            }
+        }
+        $data .=  "</pre>";
+
+        return stripcslashes($data);
     }
 }
